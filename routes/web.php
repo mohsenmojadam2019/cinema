c09288be4c8339c95755ab46ae3b3b7518f28770
mohsenmojadam <mohsenmojadam2019@gmail.com>
2026-09-20 16:54:00 +0330

    fix organization booking controller import

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\{BookingController,AdminDashboardController,TicketContr
 use App\Http\Controllers\{CustomerAuthController,CustomerAccountController};
 use App\Http\Controllers\AdminReportController;
 use App\Http\Controllers\AuthController;
-Route::get('/admin/organization-bookings',[OrganizationBookingController::class,'index'])->middleware(['auth','role:مدیر سیستم|مدیر فروش|اپراتور گیشه'])->name('admin.organization-bookings.index');
 use App\Http\Controllers\OrganizationBookingController;
+Route::get('/admin/organization-bookings',[OrganizationBookingController::class,'index'])->middleware(['auth','role:مدیر سیستم|مدیر فروش|اپراتور گیشه'])->name('admin.organization-bookings.index');
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('/search',[CatalogController::class,'search'])->name('catalog.search');
